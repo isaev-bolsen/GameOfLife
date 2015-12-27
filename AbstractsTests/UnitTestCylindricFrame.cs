7e48444ac8533454583858534658d453c66a7252
isaev-bolsen <al_mamistov@mail.ru>
2015-12-27 18:57:54 +0300

    UnitTestCylindricFrame : step 2

--- AbstractsTests/UnitTestCylindricFrame.cs
+++ AbstractsTests/UnitTestCylindricFrame.cs
@@ -18,6 +18,11 @@ namespace GameOfLife.AbstractsTests
             // 1 1 1 1
             // 0 0 0 0
 
+            // 0 0 0 0
+            // 0 0 0 0
+            // 0 0 0 0
+            // 1 1 1 1
+
             Abstracts.GameOfLifeFrame frame = new Frames.CylinderFrame(4, 4);
             for (int i = 0; i < 4; ++i) frame[i, 1] = true;
             UImock UI = new UImock() { frame = frame };
@@ -29,6 +34,14 @@ namespace GameOfLife.AbstractsTests
                     Assert.IsTrue(UI.frame[i, j]);
 
             for (int i = 0; i < 4; ++i) Assert.IsFalse(UI.frame[i, 3]);
+
+            game.Next();
+
+            for (int i = 0; i < 4; ++i)
+                for (int j = 0; j < 3; ++j)
+                    Assert.IsFalse(UI.frame[i, j]);
+
+            for (int i = 0; i < 4; ++i) Assert.IsTrue(UI.frame[i, 3]);
         }
     }
 }
